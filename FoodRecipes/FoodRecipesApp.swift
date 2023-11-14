0cc8dde37fe4f8bc6ee9fe8f5ca8685fa28f4918
winnHsu <ch4374@nyu.edu>
2023-11-14 14:59:43 -0500

    Initial Commit

---
+++ FoodRecipes/FoodRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipesApp.swift
+//  FoodRecipes
+//
+//  Created by Winn Hsu on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
